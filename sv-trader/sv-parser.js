2e49ee45aff9c9ac0990e5f6c7a77e997f1b9243
truckyand <truckyand@gmail.com>
2025-11-06 19:56:11 +0700

    Update

--- sv-trader/sv-parser.js
+++ sv-trader/sv-parser.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const pokemons = require('../sv.json');
+pokemons.sort((a, b) => a.id - b.id); // Sort Pokémon by their National Pokédex id
 console.log(`pokemons sv.json: ${pokemons.length}`);
 
 const pkmgg_sorted_pkm_json = require('../pkmgg_sorted_pokemon.json');
@@ -10,31 +11,15 @@ const pkmgg_sorted_pkm = Object.entries(pkmgg_sorted_pkm_json).map(([id, pkm]) =
 }));
 console.log(`pkmgg_sorted_pkm: ${pkmgg_sorted_pkm.length}`);
 
-/*
-pkmgg_sorted_gen8 = pkmgg_sorted_pkm.filter(pokemon => 
-	(pokemon.generation == 1)
-);
-console.log(`Gen 8 founds: ${pkmgg_sorted_gen8.length}`);
-console.log(`Gen8: ${pkmgg_sorted_gen8[0].species}`);
-*/
-
-// Sort Pokémon by their National Pokédex id
-pokemons.sort((a, b) => a.id - b.id);
-//console.log(pokemons[0]);
-
 // List of Legendary & Mythical Pokémon
 const { LegendaryMythicalList } = require('./legend.js');
-// Filter only Legendary & Mythical
-const legendary = pkmgg_sorted_pkm
-    .filter(pkm => LegendaryMythicalList.has(pkm.species.toLowerCase()))
-    //.map(([name, games]) => ({ name, games }));
-legendary.sort((a, b) => a.id - b.id);
-//console.log(legendary.map(x=>`${x.id} ${x.species}`));
-console.log(`${legendary[0].id} ${legendary[0].species}`)
+console.log(`Legendary List: ${LegendaryMythicalList.size}`);
 
-// p6
-const p6 = pokemons.slice(0, 6);
-console.log(p6.length);
+// Filter only Legendary & Mythical
+const legendary_pkm = pkmgg_sorted_pkm
+	.filter(pkm => LegendaryMythicalList.has(pkm.species))
+legendary_pkm.sort((a, b) => a.species_id - b.species_id);
+console.log(`Legendary: ${legendary_pkm.length}`);
 
 // Define chunk size
 const chunkSize = 6;
@@ -53,13 +38,13 @@ function randomDate() {
 	return `${year}${month}${day}`;
 }
 
+// p6 testing
+const p6 = pokemons.slice(0, 6);
+console.log(p6.length);
+
 // Process in chunks
 data = pokemons;
 for (let i = 0; i < data.length; i += chunkSize) {
-	//const chunk = data.slice(i, i + chunkSize);
-	//const start = i + 1;
-	//const end = Math.min(i + chunkSize, data.length);
-	//const filename = `${outputDir}/${start}-${end}.tsv`;
 
 	// Filename based on the first and last id in the chunk
 	const chunk = data.slice(i, i + chunkSize);
@@ -69,6 +54,10 @@ for (let i = 0; i < data.length; i += chunkSize) {
 
 	let content = '.bt ';
 	chunk.forEach(item => {
+		if(LegendaryMythicalList.has(item.name)) {
+			console.log(`${item.name} is legend, ignore.`);
+		}
+
 		const name = capitalize(item.name);
 		const metDate = randomDate();
 
@@ -97,7 +86,7 @@ for (let i = 0; i < data.length; i += chunkSize) {
 		else {
 			console.log(`Cannot find meta_set for ${idStr} ${name}`);
 		}
-		//content += `${meta_set}\n`;
+		content += `${meta_set}\n`;
 		content += `\n`;
 	});
 
